54f33c2bd47b66393af3259f58ab653f953e1c46
Gabriel Antonio <silvagabriel26@outlook.com>
2023-06-19 11:39:45 -0300

    python

---
+++ Python/app.py
@@ -0,0 +1,13 @@
+nome = "Pedro" String
+idade = 0 int
+altura = 0.98 float
+is_male = True / False bool
+gosto_de = "carros"
+
+print(f"Meu nome é {nome}.")
+print(f"Eu gosto de {gosto_de}")
+print(f"Eu tenho {idade} anos")
+
+idade = "35"
+
+print(f"{nome} gosta de {gosto_de} e tem {idade} anos.")
